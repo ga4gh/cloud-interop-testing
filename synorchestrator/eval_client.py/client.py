d131e1718ab35a1605868c34e6074fa7f5aaeeeb
James Eddy <james.a.eddy@gmail.com>
2018-05-10 17:06:09 -0400

    stubb out methods for eval_client module

---
+++ synorchestrator/eval_client.py/client.py
@@ -0,0 +1,39 @@
+
+
+def _configure_queue():
+    """
+    Configure external properties of a Synapse evaluation queue,
+    either based on a config file (YAML) or set of key-value pairs;
+    if the latter, save configuration to config file.
+    """
+    pass
+
+def create_queue(...):
+    """
+    Create (and configure) a new Synapse evaluation queue.
+    """
+    pass
+
+def update_queue(...):
+    """
+    Update properties or configuration for an evaluation queue.
+    """
+    pass
+
+def create_submission(...):
+    """
+    Create a new submission for an evaluation queue.
+    """
+    pass
+
+def get_submissions():
+    """
+    Retrieve submission bundles for an evaluation queue.
+    """
+    pass
+
+def list_submissions():
+    """
+    List submissions for an evaluation queue.
+    """
+    pass
