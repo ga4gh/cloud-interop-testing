bb84ee60b449eab9611402a8e31a2ca76380f11d
James Eddy <james.a.eddy@gmail.com>
2018-05-10 17:11:26 -0400

    add utility functions for building TRS and WES requests

---
+++ synorchestrator/util.py
@@ -0,0 +1,13 @@
+
+
+def build_trs_request(...):
+    """
+    Prepare Tool Registry Service request for a given submission.
+    """
+    pass
+
+def build_wes_request(...):
+    """
+    Prepare Workflow Execution Service request for a given submission.
+    """
+    pass
