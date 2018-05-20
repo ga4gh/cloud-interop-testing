b6c2f5e5b55285fbd80dc81706f5e0bbf9c68cf7
James Eddy <james.a.eddy@gmail.com>
2018-05-19 18:54:27 -0700

    use requests and endpoint urls for trs client instead of bravado/swagger

--- synorchestrator/trs_client/client.py
+++ synorchestrator/trs_client/client.py
@@ -1,24 +1,75 @@
-import urlparse
-from bravado.client import SwaggerClient
-from bravado.requests_client import RequestsClient
+import requests
+import urllib
 
 
-# TODO: this should either be an actual class or named differently
-def TRSClient(host, auth, proto):
+def _get_endpoint(client, endpoint):
+    """
+    Execute a generic API 'GET' request.
+    """
+    res = requests.get(
+        '{}/{}'.format(client.base_url, endpoint),
+        headers=client.headers
+    )
+    # TODO: add some exception handling for different responses
+    return res.json()
+
+
+def _format_workflow_id(id):
+    """
+    Add workflow prefix to and quote a tool ID.
+    """
+    return urllib.quote_plus('#workflow/{}'.format(id))
+
+
+class TRSClient(object):
     """
     Build a :class:`SwaggerClient` from a url to the Swagger
     specification for the GA4GH Tool Registry Service RESTful API.
     """
-    http_client = RequestsClient()
-    split = urlparse.urlsplit("%s://%s/" % (proto, host))
+    def __init__(self, host, auth=None, proto='http'):
+        self.base_url = '{}://{}/api/ga4gh/v2'.format(proto, host)
+        self.headers = {'Authorization': auth}
 
-    http_client.set_api_key(
-        split.hostname, auth,
-        param_name='Authorization', param_in='header'
-    )
 
-    return SwaggerClient.from_url(
-        "%s://%s/swagger.json" % (proto, host),
-        http_client=http_client,
-        config={'use_models': False, 'validate_swagger_spec': False}
-    )
+    def get_workflow(self, id):
+        """
+        Return one specific tool of class "workflow" (which has
+        ToolVersions nested inside it).
+        """
+        id = _format_workflow_id(id)
+        endpoint = 'tools/{}'.format(id)
+        return _get_endpoint(self, endpoint)
+
+
+    def get_workflow_versions(self, id):
+        """
+        Return all versions of the specified workflow.
+        """
+        id = _format_workflow_id(id)
+        endpoint = 'tools/{}'.format(id)
+        return _get_endpoint(self, endpoint)
+
+
+    def get_workflow_descriptor(self, id, version_id, type):
+        """
+        Return the descriptor for the specified workflow (examples
+        include CWL, WDL, or Nextflow documents).
+        """
+        id = _format_workflow_id(id)
+        endpoint = 'tools/{}/versions/{}/{}/descriptor'.format(
+            id, version_id, type
+        )
+        return _get_endpoint(self, endpoint)
+
+
+    def get_workflow_tests(self, id, version_id, type):
+        """
+        Return a list of test JSONs (these allow you to execute the
+        workflow successfully) suitable for use with this descriptor
+        type.
+        """
+        id = _format_workflow_id(id)
+        endpoint = 'tools/{}/versions/{}/{}/tests'.format(
+            id, version_id, type
+        )
+        return _get_endpoint(self, endpoint)
