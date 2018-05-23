1133bcd8de5fea929578bccc2eb4571577916a5e
Thomas Yu <thomasyu888@gmail.com>
2018-05-23 15:22:38 -0700

    Start filling out client.py

--- synorchestrator/eval_client.py/client.py
+++ synorchestrator/eval_client.py/client.py
@@ -1,4 +1,13 @@
+import synapseclient
+import os
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+CONFIG_FILE = os.path.join(os.path.expanduser('~'), '.evaluationConfig')
+
+syn = synapseclient.login()
 
 def _configure_queue():
     """
@@ -6,34 +15,63 @@ def _configure_queue():
     either based on a config file (YAML) or set of key-value pairs;
     if the latter, save configuration to config file.
     """
-    pass
+    quotaKeys = ['roundDurationMillis','submissionLimit','firstRoundStart','numberOfRounds']
+    try:
+        evaluation = syn.getEvaluation(evalId)
+        quota = {key:challenge_config[evalId].get(key) for key in quotaKeys if challenge_config[evalId].get(key) != "None"}
+        evaluation.quota = quota
+        if storeEvalConfig:    
+            syn.store(evaluation)
+    except Exception as exception:
+        logger.error("The evaluation queue id provided: %s, make sure your firstRoundStart configuration is in quotes or it will be read in as a datetime object" % evalId)
+        raise ValueError
+    return(evaluation)
 
 def create_queue(...):
     """
     Create (and configure) a new Synapse evaluation queue.
     """
-    pass
+
+    evalEnt = synapseclient.Evaluation(name="name",description="description",contentSource="parentId",submissionReceiptMessage="message",submissionInstructionsMessage="instructions")
+    syn.store(evalEnt)
+
 
 def update_queue(...):
     """
     Update properties or configuration for an evaluation queue.
     """
-    pass
+
+    evalEnt = syn.getEvaluation(evalId)
+    syn.store(evalEnt)
+
 
 def create_submission(...):
     """
     Create a new submission for an evaluation queue.
     """
+
     pass
 
-def get_submissions():
+def get_submissions(evaluation, status):
     """
     Retrieve submission bundles for an evaluation queue.
     """
-    pass
+    if isinstance(evaluation, basestring):
+        evaluation = syn.getEvaluation(evaluation)
+    return(syn.getSubmissionBundles(evaluation, status=status))
 
-def list_submissions():
+
+def list_submissions(evaluation, status):
     """
     List submissions for an evaluation queue.
     """
-    pass
+
+    if isinstance(evaluation, basestring):
+        evaluation = syn.getEvaluation(evaluation)
+    print '\n\nSubmissions for: %s %s' % (evaluation.id, evaluation.name.encode('utf-8'))
+    print '-' * 60
+
+    for submission, status in get_submissions(evaluation, status):
+        print submission.id, submission.createdOn, status.status, submission.name.encode('utf-8'), submission.userId
+
+
