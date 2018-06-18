06cc55606f5f402a849d016dedb1fd23ee07f063
thomasyu888 <thomasyu888@gmail.com>
2018-06-18 11:56:15 -0700

    Creation of eval

--- synorchestrator/eval_client.py/client.py
+++ synorchestrator/eval_client.py/client.py
@@ -5,8 +5,8 @@ import yml
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-CONFIG_FILE = os.path.join(os.path.expanduser('~'), './evals.yml')
-
+CONFIG_FILE = os.path.join(os.path.expanduser('~'), '.orchestratorConfig')
+EVALUATION_CONFIG = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'eval.config')
 syn = synapseclient.login()
 
 def _configure_queue(evalId, configuration = None):
@@ -15,29 +15,31 @@ def _configure_queue(evalId, configuration = None):
     either based on a config file (YAML) or set of key-value pairs;
     if the latter, save configuration to config file.
     """
+
     quotaKeys = ['roundDurationMillis','submissionLimit','firstRoundStart','numberOfRounds']
     try:
         evaluation = syn.getEvaluation(evalId)
         if configuration is not None:
-            quota = {key:challenge_config[evalId].get(key) for key in configuration if challenge_config[evalId].get(key) != "None"}
-            evaluation.quota = quota  
+            quota = {key:configuration.get(key) for key in quotaKeys}
+            evaluation.quota = quota
             syn.store(evaluation)
-            with open(CONFIG_FILE,"r") as stream:
+            with open(EVALUATION_CONFIG,"r") as stream:
                 evalConfig = yaml.load(stream)
-            evalConfig
-            data_loaded[]
-
+            if evalConfig.get(evalId) is None:
+                evalConfig[evalId] = quota
+            with open(EVALUATION_CONFIG,"w") as stream:
+                yaml.dump(evalConfig, stream, default_flow_style=False)
     except Exception as exception:
         logger.error("The evaluation queue id provided: %s, make sure your firstRoundStart configuration is in quotes or it will be read in as a datetime object" % evalId)
         raise ValueError
     return(evaluation)
 
-def create_queue(...):
+def create_queue(evaluation_name, description, contentSource, submissionReceiptMessage, submissionInstructionsMessage):
     """
     Create (and configure) a new Synapse evaluation queue.
     """
 
-    evalEnt = synapseclient.Evaluation(name="name",description="description",contentSource="parentId",submissionReceiptMessage="message",submissionInstructionsMessage="instructions")
+    evalEnt = synapseclient.Evaluation(name=evaluation_name,description=description,contentSource=contentSource,submissionReceiptMessage=submissionReceiptMessage,submissionInstructionsMessage=submissionInstructionsMessage)
     syn.store(evalEnt)
 
 
