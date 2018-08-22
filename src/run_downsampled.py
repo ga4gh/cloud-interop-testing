06ad83d225f05450ecd96c86350b39baa20bf207
DailyDreaming <lblauvel@ucsc.edu>
2018-08-22 13:17:00 -0700

    Updates.

---
+++ src/run_downsampled.py
@@ -0,0 +1,5 @@
+from synorchestrator.orchestrator import set_queue_from_user_json, run_all, run_submission
+# run_submission("Argon-Globus", "140814152850149946")
+
+set_queue_from_user_json('tests/data/user_submission_example.json')
+run_all()
