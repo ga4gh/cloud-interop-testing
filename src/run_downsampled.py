b059e90109e6bd57799ea2524a89d421d9525452
DailyDreaming <lblauvel@ucsc.edu>
2018-08-22 13:22:14 -0700

    Helium input jsons.

--- src/run_downsampled.py
+++ src/run_downsampled.py
@@ -1,4 +1,4 @@
-from synorchestrator.orchestrator import set_queue_from_user_json, run_all, run_submission
+from synorchestrator.orchestrator import set_queue_from_user_json, run_all  # , run_submission
 # run_submission("Argon-Globus", "140814152850149946")
 
 set_queue_from_user_json('tests/data/user_submission_example.json')
