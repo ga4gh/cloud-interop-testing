e5db759b9ba754af05228c18f21dee406de081f8
James Eddy <james.a.eddy@gmail.com>
2018-09-29 11:45:34 -0400

    add ability to parse env vars from yaml configs

--- wfinterop/util.py
+++ wfinterop/util.py
@@ -1,3 +1,5 @@
+import os
+import re
 import json
 import yaml
 import logging
@@ -5,6 +7,26 @@ import datetime as dt
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+def _replace_env_var(match):
+    env_var, default = match.groups()
+    return os.environ.get(env_var, default)
+
+
+def _env_var_constructor(loader, node):
+    var = re.compile(r"\$\{([^}:\s]+):?([^}]+)?\}", re.VERBOSE)
+    value = loader.construct_scalar(node)
+    return var.sub(_replace_env_var, value)
+
+
+def setup_yaml_parser():
+    var = re.compile(r".*\$\{.*\}.*", re.VERBOSE)
+    yaml.add_constructor('!env_var', _env_var_constructor)
+    yaml.add_implicit_resolver('!env_var', var)
+
+
+setup_yaml_parser()
 
 
 def heredoc(s, inputs_dict):
