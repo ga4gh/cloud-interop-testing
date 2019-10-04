9d8184afc0418b3193b921e5385087b01362ea18
Brian O'Connor <briandoconnor@gmail.com>
2019-10-04 17:41:01 -0400

    updates

--- simple/run_dashboard.sh
+++ simple/run_dashboard.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 cd $TRAVIS_BUILD_DIR/simple
-FLASK_RUN_PORT=8060 FLASK_APP=dashboard.py python -m flask run
+export FLASK_RUN_HOST=0.0.0.0
+export FLASK_RUN_PORT=8060
+export FLASK_APP=dashboard.py
+python -m flask run
--- simple/run_orchestrator.sh
+++ simple/run_orchestrator.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 cd $TRAVIS_BUILD_DIR/simple
-FLASK_RUN_PORT=8050 FLASK_APP=orchestrator.py python -m flask run
+export FLASK_RUN_HOST=0.0.0.0
+export FLASK_RUN_PORT=8050
+export FLASK_APP=orchestrator.py 
+python -m flask run
